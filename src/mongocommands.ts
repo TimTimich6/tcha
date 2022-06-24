14d5601674280468808b0825c778a2e080b36c76
Tim <timofeyobraztsov@gmail.com>
2022-06-23 17:14:59 -0700

    email validation

--- src/mongocommands.ts
+++ src/mongocommands.ts
@@ -1,4 +1,4 @@
-import { MongoClient, WithId } from "mongodb";
+import { Document, MongoClient, WithId } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 const uri: string = `mongodb+srv://tim:${process.env.MONGOPASS}@cluster0.k1aaw.mongodb.net/chattingapp?retryWrites=true&w=majority`;
@@ -10,14 +10,18 @@ const client = new MongoClient(uri);
 })();
 
 export const createUser = async (username: string, password: string) => {
-  const result = await client.db("chattingapp").collection("users").insertOne({
+  await client.db("chattingapp").collection("users").insertOne({
     username: username,
     password: password,
   });
+};
+
+export const loginUser = async (username: string, password: string): Promise<null | WithId<Document>> => {
+  const result = await client.db("chattingapp").collection("users").findOne({ username, password });
   return result;
 };
 
-export interface newUser {
+export interface UserQuery {
   username: string;
   password: string;
 }
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import * as db from "./mongocommands";
 import cors from "cors";
 import dotenv from "dotenv";
+import axios from "axios";
 dotenv.config();
 const app = express();
 app.use(cors());
@@ -9,18 +10,31 @@ app.use(express.json());
 const PORT = process.env.PORT || 3080;
 
 app.post("/api/signup", async (req, res) => {
-  console.log("received /api");
-  const body: db.newUser = req.body;
+  console.log("received post /api/signup");
+  const body: db.UserQuery = req.body;
   console.log("data", body);
   try {
-    await db.createUser(body.username, body.password);
-    res.json({ message: "created user", user: body });
+    const response = await axios.get(`https://api.eva.pingutil.com/email?email=${body.username}`);
+    if (response.data.status == "success") {
+      await db.createUser(body.username, body.password);
+      res.json({ message: "created user", user: body });
+    } else throw new Error();
   } catch (error) {
     res.json({ message: "failed to create user" });
   }
 });
 
-app.post("/api/login", async (req, res) => {});
+app.post("/api/login", async (req, res) => {
+  console.log("received post /api/login");
+  const body: db.UserQuery = req.body;
+  try {
+    const response = await db.loginUser(body.username, body.password);
+    if (response == null) throw { message: "null value" };
+    res.json({ message: "successful signin", time: Date.now() });
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 app.post("/api", async (req, res) => {
   console.log("received /api");
